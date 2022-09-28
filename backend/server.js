123e960e96a1b49f17d1ec558ddc7623d0fb1d93
chinmay <chinmaymhatre12@gmail.com>
2022-09-28 16:59:32 +0530

    build

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // express server
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,5 +20,16 @@ app.use(errorHandler);
 app.use("/api/tasks", require("./routes/taskRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/practise", require("./routes/practiseRoutes"));
+
+// server front end
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname,'../', 'frontend', 'build', 'index.html')));
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...');
+    });
+}
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
